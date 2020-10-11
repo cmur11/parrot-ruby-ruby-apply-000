3b8fe70f495fab839669e247936414d9af8c680b
Conor Murnane <conormurnane11@gmail.com>
2020-10-11 20:04:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,16 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase = "Squawk!")
+    puts "#{phrase}"
+end
+
+puts parrot("Pretty bird!")
+
+
+
+def say_hello(name = "Ruby Programmer")
+    puts "Hello #{name}!"
+end
+
+puts say_hello("Gabriela!")
